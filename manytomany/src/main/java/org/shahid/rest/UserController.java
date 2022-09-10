1f89a58763753f269a15fb6ee157782407757da3
Shaikh Shahid Iqbal <35321907+nilgadia@users.noreply.github.com>
2022-09-10 21:21:42 +0300

    Update UserController.java
    Added method to create user

--- manytomany/src/main/java/org/shahid/rest/UserController.java
+++ manytomany/src/main/java/org/shahid/rest/UserController.java
@@ -6,9 +6,7 @@ import org.shahid.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +19,12 @@ public class UserController {
     private final UserService service;
 
     @GetMapping()
-    public ResponseEntity<List<UserDto>> getAllQueries() {
+    public ResponseEntity<List<UserDto>> getAllUsers() {
         return new ResponseEntity<>(this.service.getAllUsers(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
+        return new ResponseEntity<>(this.service.save(user), HttpStatus.OK);
     }
 }
